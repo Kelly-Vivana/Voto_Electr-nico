b9b1d17f7d127089d1983eb59ca2dfb7d031a328
Kelly <77462355+Kelly-Vivana@users.noreply.github.com>
2021-09-08 17:30:30 -0500

    eliminacion de archivos innecesarios

--- login.js
+++ login.js
@@ -1,8 +1,15 @@
 //archivos importados
-const { use } = require('./src/routes/index.js');
+
 const app= require('./src/config/server.js');
 app.use= require('./src/routes/index');
 
+//Inicializaciones (mando a ejecutar express y devuelve la app)
+const app =express();
+
+//configuracion de la app
+app.set('port', process.env.PORT || 3000); // si el servicio de la nube da un puerto usalo, caso contrario usa el 3000
+app.set('views', path.join( __dirname, '../app/views'));
+app.set('views', 'ejs');
 
 // app.use(express.static('public/index.html'));
 
