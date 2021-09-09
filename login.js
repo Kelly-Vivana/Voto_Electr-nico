19f797f676e6246421a5c259d9f2c6bf4582960b
Kelly <77462355+Kelly-Vivana@users.noreply.github.com>
2021-09-09 09:26:06 -0500

    presentacion de login con sus imagenes

--- login.js
+++ login.js
@@ -1,25 +1,38 @@
 //archivos importados
-
-const app= require('./src/config/server.js');
-app.use= require('./src/routes/index');
-
+const express= require('express');
 //Inicializaciones (mando a ejecutar express y devuelve la app)
 const app =express();
+const mysql= require('mysql') //encargado de hacer peticiones
+const path= require('path');
+
+
+console.log(path.join(__dirname, 'src/public'));
 
 //configuracion de la app
 app.set('port', process.env.PORT || 3000); // si el servicio de la nube da un puerto usalo, caso contrario usa el 3000
-app.set('views', path.join( __dirname, '../app/views'));
-app.set('views', 'ejs');
+app.set('views', path.join( __dirname, '/src/views'));
+app.set('view engine', 'ejs');
 
-// app.use(express.static('public/index.html'));
+//Middleware
+app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(myConnection(mysql, {
+    host:'localhost',
+    user:'root',
+    password:'',
+    port: 3306,
+    database: 'votaciones'
+}, 'single' ));
 
-//Configuracion
 
-// app.use(express.static('src'));
+//Variables globales
+
+//Routes
+app.use(require('./src/routes/index'));
 
 
+//Acceso a los archivos publicos
 
+//iniciar el servidor
 app.listen(app.get('port'), ()=>{ //obtiene el puerto
     console.log("port ", app.get('port'));
 });
-
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,15 @@
 const express= require('express');
 const router= express.Router(); //modulo router pertenece a express, es encargado de crear las rutas
 
-const app= express();
-
-app.get('/', (req,res)=>{
+router.get('/', (req,res)=>{
     res.render('index');
- });
+});
+router.get('/', (req,res)=>{
+    res.sendFile('img');
+});
 
- app.get('/registro', (req,res)=>{
-    res.render('registro', {title: Registrarse});
- });
+//  app.get('/registro', (req,res)=>{
+//     res.render('registro', {title: Registrarse});
+//  });
 
- module.exports=router;
+ module.exports = router;
